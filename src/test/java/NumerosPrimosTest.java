d80d012608a3ba40ba422dcccc01df6d14b76523
fcardenas07 <f.cardenasreyes.94@gmail.com>
2023-03-29 10:20:39 -0300

    Se modifica esPrimoTest()

--- src/test/java/NumerosPrimosTest.java
+++ src/test/java/NumerosPrimosTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NumerosPrimosTest {
@@ -9,15 +10,18 @@ class NumerosPrimosTest {
 
     @BeforeEach
     public void init() {
-        numeros = new int[]{-1, 0, 1, 2, 3, 4, 5, 6, 7, 11, 13, 97};
-        resultadosEsperados = new boolean[]{false, false, false, true, true, false, true, false, true, true, true, true};
+        numeros = new int[]{-1, 0, 1, 2, 3, 4, 5, 6, 7, 11, 13, 97, 479001599};
+        resultadosEsperados = new boolean[]{true, false, false, true, true, false, true, false, true, true, true, true, true};
     }
 
     @Test
     public void esPrimoTest() {
+        boolean[] resultadosObtenidos = new boolean[numeros.length];
+
         for (int i = 0; i < numeros.length; i++) {
-            boolean resultadoObtenido = NumerosPrimos.esPrimo(numeros[i]);
-            assertEquals(resultadoObtenido, resultadosEsperados[i]);
+            resultadosObtenidos[i] = NumerosPrimos.esPrimo(numeros[i]);
+
         }
+        assertArrayEquals(resultadosEsperados, resultadosObtenidos);
     }
 }
